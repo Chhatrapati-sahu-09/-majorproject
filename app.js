fac98d92b056e7ca5ea92f8625d8547d4ddbe638
Chhatrapati sahu <chhatrapatisahu09@gmail.com>
2026-02-14 20:17:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const userRouter = require("./routes/user.js");
 
-//==============================================================
+//====================================================
 
 const dbUrl = process.env.ATLASDB_URL;
 
